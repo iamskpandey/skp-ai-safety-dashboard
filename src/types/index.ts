c2935fe81fc288ab3bc36479c5b7bafc8e2c72c0
iamskpandey <188148706+iamskpandey@users.noreply.github.com>
2025-05-01 21:04:04 +0530

    Remove unused type definitions for pages and routes

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,5 +1,3 @@
 export * from './incident';
 export * from './components';
 export * from './layouts';
-export * from './pages';
-export * from './routes'; 
--- src/types/routes.ts
+++
@@ -1,6 +0,0 @@
-import { Incident } from './incident';
-
-export interface RoutesProps {
-  incidents: Incident[];
-  onAddIncident: (newIncident: Omit<Incident, 'id'>) => void;
-} 
